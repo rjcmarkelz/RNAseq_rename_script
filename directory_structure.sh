0a5aeda56c6fbf9e95423802965607d3299939e4
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-04-21 23:51:23 -0700

    update symbolic links script

--- directory_structure.sh
+++ directory_structure.sh
@@ -5,5 +5,8 @@
 cd /path/to/directories &&
 find . -type d -exec mkdir -p -- /path/to/backup/{} \;
 
-ln -s /Users/Cody_2/git.repos/RILS/Block1/project.maloof/ &&
+ln -s /Users/Cody_2/git.repos/RILS/Block1/project.maloof/
 project.maloof.link
+
+ln -s -f /Users/Cody_2/git.repos/RILS/Block1/project.maloof/RIL_2.fq
+  /Users/Cody_2/git.repos/RILS_2/Block1/project.maloof/RIL_2_new.fq
