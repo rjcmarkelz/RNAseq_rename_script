bca765730db0af4f1f9e35611bdab0b990ecc382
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-04-23 19:00:21 -0700

    updated infile commands

--- infile_csv.py
+++ infile_csv.py
@@ -1,18 +1,37 @@
 
+# import csv
+# test_file = 'block_2_3_nocontaminants_play.csv'
+# csv_file  = csv.DictReader(open(test_file, 'rU'), delimiter = ',', quotechar='"')
+
+# for line in csv_file:
+# 	print line
+
+
+
+
+
+
 import csv
-test_file = 'block_2_3_nocontaminants_play.csv'
-csv_file  = csv.DictReader(open(test_file, 'rU'), delimiter = ',', quotechar='"')
+import os
 
-for line in csv_file:
-	print line
-
-
-import csv
 with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
+    	# print row[0]
+    	source_name = path/row[0].fq
+    	link_name = path/row[1].rn.fq
+    	os.symlink(source_name, link_name)
+    	# for i in row:
+    		# print i
+    	# print ' 1  2   3  '.split()
         print row
 
-with open("block_2_3_nocontaminants_play.csv") as fd:
-    d = dict(line.strip().split(',', 1) for line in fd)
-    print d
+# import csv
+# with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print row
+
+# with open("block_2_3_nocontaminants_play.csv") as fd:
+#     d = dict(line.strip().split(',', 1) for line in fd)
+#     print d
