506923a230b8821e54dd3e165cf31665170528cb
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-04-23 17:18:49 -0700

    playing with infile functions

--- infile.py
+++ infile.py
@@ -1,8 +1,26 @@
 
-ins = open( "block_2_3_nocontaminants_play.txt", "r" )
-array = []
-for line in ins:
-    array.append( line )	
-print ins
-ins.close()
+InFile = file('block_2_3_nocontaminants_play.txt', 'r')
+content = InFile.read()
+InFile.close()
+
+print content
+#only prints the last line from the file
+
+#a = dict(open('block_2_3_nocontaminants_play.txt'))
+#print a
+
+
+# dict takes a sequence of  `(key, value)` pairs and turns in into a dict
+#print dict(get_entries( infile ))
+
+f = open('block_2_3_nocontaminants_play.txt')
+for line in f:
+	print line,
+f.close()
+
+
+with open('block_2_3_nocontaminants_play.txt', 'r') as f:
+	
+	for line in f:
+		print line,
 
---
+++ infile_csv.py
@@ -0,0 +1,18 @@
+
+import csv
+test_file = 'block_2_3_nocontaminants_play.csv'
+csv_file  = csv.DictReader(open(test_file, 'rU'), delimiter = ',', quotechar='"')
+
+for line in csv_file:
+	print line
+
+
+import csv
+with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row
+
+with open("block_2_3_nocontaminants_play.csv") as fd:
+    d = dict(line.strip().split(',', 1) for line in fd)
+    print d
