cb2460a677a7dcf11456648c0a81da5c2a50cb90
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-04-25 10:39:31 -0700

    infile csv to dictionary

--- infile_csv.py
+++ infile_csv.py
@@ -14,17 +14,17 @@
 import csv
 import os
 
-with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
-    reader = csv.reader(f)
-    for row in reader:
-    	# print row[0]
-    	source_name = path/row[0].fq
-    	link_name = path/row[1].rn.fq
-    	os.symlink(source_name, link_name)
-    	# for i in row:
-    		# print i
-    	# print ' 1  2   3  '.split()
-        print row
+# with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#     	# print row[0]
+#     	source_name = path/row[0].fq
+#     	link_name = path/row[1].rn.fq
+#     	os.symlink(source_name, link_name)
+#     	# for i in row:
+#     		# print i
+#     	# print ' 1  2   3  '.split()
+#         print row
 
 # import csv
 # with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
@@ -32,6 +32,13 @@ with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
 #     for row in reader:
 #         print row
 
-# with open("block_2_3_nocontaminants_play.csv") as fd:
-#     d = dict(line.strip().split(',', 1) for line in fd)
-#     print d
+
+
+with open("block_2_3_nocontaminants_play.csv") as fd:
+    d = dict(line.strip().split(',', 1) for line in fd)
+    print d
+
+
+
+
+
