646353f9c3b7577809226e68009de7fe79305f3f
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-04-26 16:47:23 -0700

    add sys.argv for providing the paths

--- dict_os_regex_infile.py
+++ dict_os_regex_infile.py
@@ -2,19 +2,29 @@
 import os 
 import re
 import csv
-
+import sys
 
 ######################################################
 ######################################################
-# Open a file
-#cody_2 path laptop
-#path = "/Users/Cody_2/git.repos/RILS/Block1/project.maloof/"
-#path2 = "/Users/Cody_2/git.repos/RILS/Block1/project.maloof.renamed/"
+# Provide Pathnames for Target Folder and Symbolic
+# Link Folder
+######################################################
+######################################################
+
+block_num = sys.argv[1]
+path = "/Users/Cody_2/git.repos/RILS/Block%s/project.maloof/" % (block_num)
+path2 = "/Users/Cody_2/git.repos/RILS/Block%s/project.maloof.renamed/" % (block_num)
+
+# path  = sys.argv[1]
+# path2  = sys.argv[2]
 
 #cody_1 path home imac
-path  = "/Users/Cody/Documents/Maloof Lab/My Brassica/Block2/project.maloof/"
-path2 = "/Users/Cody/Documents/Maloof Lab/My Brassica/Block2/project.maloof.rename/"
+# path  = "/Users/Cody/Documents/Maloof Lab/My Brassica/Block2/project.maloof/"
+# path2 = "/Users/Cody/Documents/Maloof Lab/My Brassica/Block2/project.maloof.rename/"
 
+#cody_2 path laptop
+# path = "/Users/Cody_2/git.repos/RILS/Block1/project.maloof/"
+# path2 = "/Users/Cody_2/git.repos/RILS/Block1/project.maloof.renamed/"
 
 # #user input
 # path  = raw_input("Enter the absolute path for the input directory: ")
@@ -30,10 +40,29 @@ path2 = "/Users/Cody/Documents/Maloof Lab/My Brassica/Block2/project.maloof.rena
 # print "------------------"
 
 pathfiles = os.listdir(path)
+
+
 ###################################################
 ###################################################
 
+#Small play dictionary for quick testing
+
+RN_Dict = {
+'RIL_360.02' :'RIL_1',
+'RIL_73.02'  :'RIL_4',
+'RIL_259.0' :'RIL_103(contaminated?)',
+'RIL_251' :'RIL_IMB211',
+'RIL_341' :'SIG_CON',
+'RIL_265' :'RIL_113.rn',
+}
+
+# print RN_Dict
+# keys   = RN_Dict.keys()
+# values = RN_Dict.values()
+# print keys
+# print values
 # RN_Dict = dict()
+
 # with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
 #     reader = csv.reader(f)
 #     for row in reader:
@@ -42,24 +71,25 @@ pathfiles = os.listdir(path)
 #         RN_Dict[old] = new
 #         print RN_Dict
 
-#Small play dictionary for quick testing
+re12 = '^RIL'
+re13 = '_' 
+re14 = '([A-Z0-9]+$)'
+rg2 = re.compile(re12+re13+re14,re.IGNORECASE|re.DOTALL)
 
-RN_Dict = {
-'RIL_360.12' :'RIL_1(contaminated?)',
-'RIL_73'  :'RIL_4',
-'RIL_259' :'RIL_103(contaminated?)',
-'RIL_251' :'RIL_104',
-'RIL_113' :'SIG_CON',
-'RIL_265' :'RIL_113.rn',
-}
+Uncon_Dict = {}
+Con_Dict  = {}
 
-print RN_Dict
-keys   = RN_Dict.keys()
-values = RN_Dict.values()
-print keys
-print values
-
-
+for key in RN_Dict:
+    match = rg2.search(RN_Dict[key])
+    if match:
+        Uncon_Dict[key] = RN_Dict[key]
+        print "Uncontaminated: ", RN_Dict[key]
+    else:
+        Con_Dict[key] = RN_Dict[key]
+        print "Contaminated: ", RN_Dict[key]
+        
+print Uncon_Dict
+print Con_Dict
 ######################################################
 ######################################################
 
@@ -69,11 +99,11 @@ print values
 re1 = '.*?'     # Match frontend
 re2 = '(Blk)'   # Blk
 re3 = '(\\d+)'  # Blk_number
-re4 = '(.)'     # underscore (any character)
+re4 = '(_)'     # underscore 
 re5 = '(RIL)'   # RIL
-re6 = '(_)'     # underscore (any character)
+re6 = '(_)'     # underscore 
 re7 = '([A-Z0-9]+)'  # RIL_number
-re8 = '(.)'                 # underscore (any character)
+re8 = '(_)'                 # underscore
 re9='((?:[a-z][a-z]+))'     # Treatment
 re10='(.)'                  # dot (any character)
 re11='((?:[a-z][a-z]+))'    # fq 
@@ -83,8 +113,8 @@ rg = re.compile(re1+re2+re3+re4+re5+re6+re7+re8+re9+re10+re11,re.IGNORECASE|re.D
 ######################################################
 ######################################################
 
-for file in pathfiles:
-    source_name = file
+for fn in pathfiles:
+    source_name = fn
     print source_name
     match = rg.search(source_name)
     if match:
@@ -104,19 +134,21 @@ for file in pathfiles:
         conv_Blk_num = '.' + str(Blk_number).zfill(2)
         print conv_Blk_num
 
-        capture = Blk + Blk_number + underscore1 + RIL + underscore2 + RIL_num + underscore3 + treatment + dot + fq
-        print capture
+        # capture = Blk + Blk_number + underscore1 + RIL + underscore2 + RIL_num + underscore3 + treatment + dot + fq
+        # print capture
+
         oldname = RIL + underscore2 + RIL_num
         print oldname
+        
         RIL_string = RIL + underscore2 + RIL_num + conv_Blk_num
         print RIL_string
         
-        if RIL_string in RN_Dict:
-            print "Making RIL name replacement. \n Old Name: %s; \n New Name: %s" % (oldname, RN_Dict[RIL_string])
-            link_name = 'RN_' + source_name.replace(oldname, RN_Dict[RIL_string])
+        if RIL_string in Uncon_Dict:
+            print "Making RIL name replacement. \n Old Name: %s; \n New Name: %s" % (oldname, Uncon_Dict[RIL_string])
+            link_name = 'RN_' + source_name.replace(oldname, Uncon_Dict[RIL_string])
             print "Symbolic link will have name:", link_name
-            symlinktarget = path  + source_name
-            symlinkpath   = path2 + link_name
+            symlinktarget = path  + "/" + source_name
+            symlinkpath   = path2 + "/" + link_name
             print symlinktarget
             print symlinkpath
             os.symlink(symlinktarget, symlinkpath)
