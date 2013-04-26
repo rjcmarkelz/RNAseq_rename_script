ff708e0f6942d3b063d1ac093c9f0530bb3fbc04
Cody Markelz <rjmarkelz@ucdavis.edu>
2013-04-26 16:45:45 -0700

    infile script cleanup

--- infile_csv_working.py
+++ infile_csv_working.py
@@ -1,17 +1,17 @@
 import csv
 
 data = dict()
-with open('block_2_3_nocontaminants_play.csv', 'rU') as f:
+with open('entire_rename_list.csv', 'rU') as f:
     reader = csv.reader(f)
     for row in reader:
         old = row[0]
         new = row[1]
         data[old] = new
-        print data
+        #print data
 
 
-
-
+length = len(data)
+print length #1022
 
 
 
